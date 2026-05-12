86484f438908709c79638907678290940d7fc53f
Caua <cauapxa@gmail.com>
2026-05-11 21:36:12 -0300

    Reestruturação da main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from libraries import face_recognition_lib
-
-face_recognition_lib.gerar_embedding("data/individual/caua_teste.jpeg", "caua", "202411250036")
-face_recognition_lib.gerar_embedding("data/individual/murilo_teste.jpeg", "murilo", "2024")
-print(face_recognition_lib.gerar_embedding("data/individual/murilo_teste.jpeg", "murilo", "202412314"))
-
-print(face_recognition_lib.comparar_embedding("data/turma/trio.jpeg"))
